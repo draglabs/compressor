9579680903582484ca015d7ed36154aec3bafd0c
Marlon Monroy <monroy@Marlons-MacBook-Pro.local>
2017-10-25 18:41:37 -0700

    should display

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"compressor/routes"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -13,6 +14,7 @@ func main() {
 	mux.HandleFunc("/", routes.Index)
 	mux.Handle("/archive", handler)
 	log.Fatal(http.ListenAndServe(":8081", mux))
+	fmt.Println("running on")
 }
 
 var mux = http.NewServeMux()
