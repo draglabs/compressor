eea8d7de240855249c4c8be6e67765d94a863a03
Marlon Monroy <monroy@Marlons-MacBook-Pro.local>
2017-10-26 01:31:12 -0700

    changed the region

--- main.go
+++ main.go
@@ -5,16 +5,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
-//var port = os.Getenv("PORT")
+var port = os.Getenv("PORT")
 
 func main() {
 	handler := routes.NewArchiveRouter()
 	mux.HandleFunc("/", routes.Index)
 	mux.Handle("/archive", handler)
 	fmt.Println("running on")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(http.ListenAndServe(port, mux))
 
 }
 
