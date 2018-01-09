2c713953f09b14bee459e27ac5b2c7fbbcf8f32e
Marlon Monroy <monroy@Marlons-MacBook-Pro.local>
2018-01-09 13:51:43 -0800

    debuging

--- controllers/jam.go
+++ controllers/jam.go
@@ -133,7 +133,7 @@ func archiveIfNeeded() error {
 		fmt.Println("error from generating", err)
 		return err
 	}
-	if err := archiver.ZipArchive(currentJam.Name, "archive.zip"); err == nil {
+	if err := archiver.ZipArchive(".uploads", "archive.zip"); err == nil {
 
 		url, err := uploader.Upload("archive.zip", currentJam.Name)
 		if err == nil {
